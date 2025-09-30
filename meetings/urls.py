28d0899b9163d34767fec049473ae4522e7b5d09
chadirabii <chadirabii@gmail.com>
2025-09-30 13:41:51 +0100

    Add meetings list view and template; remove unused all_meetings.html

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from meetings.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('website/', include('website.urls')),  # new
+    path('website/', include('website.urls')),
+    path('getAll/', meetings_list_view, name='meetings_list')
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from meetings.models import Meeting
+
 # Create your views here.
+
+
+def meetings_list_view(request):
+    meetings = Meeting.objects.all()
+    return render(request, "meetings.html", {"meetings": meetings})
